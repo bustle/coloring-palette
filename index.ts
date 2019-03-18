945bd8dbc9900939b5c39b0f7493f13b700cefde
tristan <tristan.t.siegel@gmail.com>
2019-03-18 11:51:05 -0400

    wip

--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
-import { TinyColor } from '@ctrl/tinycolor'
+import { TinyColor } from '@ctrl/tinycolor/'
+
+function calcShortestDistance(a, b) {
+  const difference = Math.abs(a - b) % 360
+  return difference > 180 ? 360 - difference : difference
+}
 
 export default function (color: string) {
 
